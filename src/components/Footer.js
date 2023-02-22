89e6d53edca4ef2e03d20857a7d7e051882b4d2a
Omkar0803 <97947439+Omkar0803@users.noreply.github.com>
2023-02-22 23:14:56 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import { Github } from "../assets";
 const Footer = () => {
   return (
     <div className="flex justify-center items-center gap-2 mb-5 text-[1.1rem]">
-      <a href="https://github.com/Omkar0803/Clarity-hackathon" target="_blank">
+      <a href="https://github.com/Omkar0803/filechain" target="_blank">
         <Github className="w-7" />
       </a>
       | Made with ❤️ at EthforAll
